4fffcc9809e36f9e588cc354e3934478c4a67451
xyz <asd@asd.asd>
2021-02-07 17:51:40 +0100

    my notes

--- 02-schema/src/app.js
+++ 02-schema/src/app.js
@@ -1,41 +1,45 @@
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer } = require("graphql-yoga");
 
 const moviesList = [
-  { id: 1, title: 'Peaceful Warrior', duration: 0 },
-  { id: 2, title: 'Gone in Sixty Seconds', duration: 60 },
-  { id: 3, title: 'Matrix', duration: 404 }
+  { id: 1, title: "Peaceful Warrior", duration: 0, duration: 129 },
+  { id: 2, title: "Gone in Sixty Seconds", duration: 60, duration: 145 },
+  { id: 3, title: "Matrix", duration: 404, duration: 185 },
 ];
 
 const songsList = [
-  { id: 1, title: 'Heart of courage'},
-  { id: 2, title: 'For the Win'},
-  { id: 3, title: 'Immortal'}
-]
+  { id: 1, title: "Heart of courage" },
+  { id: 2, title: "For the Win" },
+  { id: 3, title: "Immortal" },
+];
 
 const resolvers = {
-  // Media: {
-    // __resolveType(obj, context, info) {
-      // return 'Movie';
-    // }
-  // },
-  Query: {
-    demo: () => 'Hello GraphQL Schema!',
-    // movies: () => moviesList,
-    // movie: (parent, args) => moviesList.find(movie => movie.id == args.id),
-    // song: (parent, args) => songsList.find(song => song.id == args.id)
+  Media: {
+    __resolveType(obj, context, info) {
+      return "Movie";
+    },
   },
-  // Mutation: {
-    // createMovie: (parent, args) => {
-      // const id = moviesList.length + 1
-      // const movie = { id: id, title: args.movie.title, director: args.movie.director }
-      // moviesList.push(movie);
-      // return movie;
-    // }
-  // }
-}
+  Query: {
+    demo: () => "Hello GraphQL Schema!",
+    movies: () => moviesList,
+    movie: (parent, args) => moviesList.find((movie) => movie.id == args.id),
+    song: (parent, args) => songsList.find((song) => song.id == args.id),
+  },
+  Mutation: {
+    createMovie: (parent, args) => {
+      const id = moviesList.length + 1;
+      const movie = {
+        id: id,
+        title: args.movie.title, // jeśli w Mutation podajesz jako argument jakiś obiekt np:   createMovie(movie: MovieInput): Movie! to wtedy to movie staje się obiektem więc aby np dostać title musisz napisać args.movie.title gdzie to 'title' z 'args' to nazwa w mutation --^ . Jakbyś podawał ręcznie wszystkie pola, np: createMovie(title: "adas" duration: "436356") to wtedy nie trzeba odwoływać się do obiektu movie no bo nie twrzysz tylko bezpośrednio przekazujesz te dane
+        duration: args.movie.duration,
+      };
+      moviesList.push(movie);
+      return movie;
+    },
+  },
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
 });
 
--- 05-database/src/app.js
+++ 05-database/src/app.js
@@ -1,6 +1,13 @@
-const { ApolloServer, gql } = require('apollo-server');
-const { readFileSync } = require('fs');
-const { prisma } = require('./generated/prisma-client');
+const { ApolloServer, gql } = require("apollo-server");
+const { readFileSync } = require("fs");
+const { prisma } = require("./generated/prisma-client");
+
+// ZAMIAST GraphQL-Yoga będziemy korzystać z Apollo ponieważ nowe wersje Apollo wprowadizły takie zmiany że korzystanie z graphql-yoga nie ma już sensu i mozna od razu korzystać z Apollo
+
+// usunęliśmy także te tablice z danymi ponieważ zastąpimy je prawdziwą bazą danych
+// yarn add apollo-server graphql prisma-client-lib
+// yarn global add prisma
+// yarn remove graphql-yoga <--- usuwamy graphql-yoga bo już nie będzie potrzebny
 
 const resolvers = {
   Query: {
@@ -11,40 +18,48 @@ const resolvers = {
     },
     director: async (parent, args, context, info) => {
       return prisma.director({ id: args.id });
-    }
+    },
   },
   Mutation: {
-    createMovie: async(parent, args) => {
-      const movie = { title: args.title, duration: args.duration, director: {
-        connect: { id: args.director }
-      } };
+    createMovie: async (parent, args) => {
+      const movie = {
+        title: args.title,
+        duration: args.duration,
+        director: {
+          connect: { id: args.director },
+        },
+      };
       return prisma.createMovie(movie);
     },
-    createDirector: async(parent, args) => {
+    createDirector: async (parent, args) => {
       const director = { name: args.name };
       return prisma.createDirector(director);
-    }
+    },
   },
   Movie: {
     duration: ({ duration }, args) => {
-      if (args.unit == 'MINUTES') return Math.round(duration / 60);
-      if (args.unit == 'HOURS') return Math.round(duration / 3600);
+      if (args.unit == "MINUTES") return Math.round(duration / 60);
+      if (args.unit == "HOURS") return Math.round(duration / 3600);
 
       return duration;
     },
     director: async (parent, args, context, info) => {
-      return prisma.movie({ id: parent.id }).director()
-    }
+      return prisma.movie({ id: parent.id }).director();
+    },
   },
   Director: {
     movies: (parent, args, context, info) => {
-      return moviesList.filter(movie => movie.director_id == parent.id);
-    }
-  }
-}
+      return moviesList.filter((movie) => movie.director_id == parent.id);
+    },
+  },
+};
 
+// tu jest zmiana -> odczytujemy plik z schema a wczesniej podawaliśmy tylko ściezkę do niego bo graphql-yoga czytał te pliki .graphql
 const server = new ApolloServer({
-  typeDefs: gql`${readFileSync(__dirname.concat('/schema.graphql'), 'utf8')}`,
+  // poniżej dla typeDefs wczesniej podawaliśmy ściezkę do pliku ale teraz musimy odczytać jego zawartość
+  typeDefs: gql`
+    ${readFileSync(__dirname.concat("/schema.graphql"), "utf8")}
+  `,
   resolvers,
 });
 
